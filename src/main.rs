4475ba35e272b7fc9b9daad4372ff4229b580524
quyc <quycsff@gmail.com>
2024-01-26 23:06:07 +0800

    log

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use std::fmt::Debug;
 use axum::Router;
 use axum::routing::get;
 use tokio::net::TcpListener;
 use tracing::info;
+use tracing::log::LevelFilter;
 use tracing_appender::rolling;
 use tracing_subscriber::EnvFilter;
 use tracing_subscriber::fmt::writer::MakeWriterExt;
@@ -13,7 +15,7 @@ use chat_server::user::UserApi;
 
 #[tokio::main]
 async fn main() {
-    log_init_non_block().await;
+    log_init_multi().await;
     color_eyre::install().unwrap();
     info!("chat server start begin!");
     let app_state = AppState::new().await.unwrap();
@@ -27,7 +29,7 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn log_init() {
+async fn log_file() {
     // 输出到文件
     let debug_file = rolling::daily("./logs", "debug");
     let warn_file = rolling::daily("./logs", "warn");
@@ -39,41 +41,22 @@ async fn log_init() {
         .with_ansi(false)
         .init();
 
-    // // 输出到控制台
-    // tracing_subscriber::fmt().pretty().with_writer(std::io::stdout).init();
-    // // 输出到文件
-    // let debug_file = rolling::daily("./logs", "debug");
-    // let warn_file = rolling::daily("./logs", "warn");
-    // let (non_blocking_debug, _guard) = tracing_appender::non_blocking(debug_file);
-    // let (non_blocking_warn, _guard) = tracing_appender::non_blocking(warn_file);
-    // let all_logs = non_blocking_debug.with_max_level(tracing::Level::TRACE).and(non_blocking_warn.with_max_level(tracing::Level::WARN));
-    // tracing_subscriber::fmt()
-    //     .with_writer(all_logs)
-    //     .with_ansi(false)
-    //     .init();
-    // let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(tracing::Level::INFO.as_str()));
-    // // Registry::default()
-    // //     .with(env_filter)
-    // //     .with(console_layer)
-    // //     .init()
 }
 
-async fn log_init_non_block() {
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    // 输出到控制台中
-    let formatting_layer = tracing_subscriber::fmt::layer().pretty().with_writer(std::io::stderr);
-
-    // 输出到文件中
-    let file_appender = rolling::never("logs", "app.log");
-    // let (non_blocking_appender, _guard) = tracing_appender::non_blocking(file_appender);
-    let file_layer = tracing_subscriber::fmt::layer()
-        .with_ansi(false)
-        .with_writer(file_appender);
-
-    // 注册
-    tracing_subscriber::Registry::default()
-        .with(env_filter)
-        .with(formatting_layer)
-        .with(file_layer)
-        .init();
+async fn log_init_multi() {
+    let file_appender = rolling::hourly("logs", "info.log");
+    // 不生效，不知道为什么
+    // let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    tracing_subscriber::registry()
+        .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(tracing::Level::INFO.as_str())))
+        .with(tracing_subscriber::fmt::layer().with_writer(file_appender)
+            .with_ansi(false)
+            .pretty())
+        .with(tracing_subscriber::fmt::layer()
+            .with_writer(std::io::stdout)
+            .with_line_number(true)
+            .with_thread_ids(true)
+            .with_thread_names(true)
+            .pretty())
+        .init()
 }
