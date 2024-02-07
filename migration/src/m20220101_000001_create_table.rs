9198832d2423ccfcb8e38d0ab5f9e8c0e24a910d
quyc <quycsff@gmail.com>
2024-02-08 00:26:37 +0800

    migration

--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -8,7 +8,7 @@ impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         let db = manager.get_connection();
         db.execute_unprepared("
-        CREATE TABLE `user1` (
+        CREATE TABLE `user` (
           `id` int NOT NULL AUTO_INCREMENT,
           `name` varchar(255) NOT NULL,
           `email` varchar(255) NOT NULL,
@@ -16,7 +16,7 @@ impl MigrationTrait for Migration {
           `password` varchar(255) NOT NULL,
           `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
           `update_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-          `status` tinyint NOT NULL DEFAULT '0' COMMENT '状态：0:正常，1:冻结',
+          `status` enum('NORMAL','FREEZE') NOT NULL DEFAULT 'NORMAL' COMMENT '状态：正常，冻结',
           PRIMARY KEY (`id`)
         ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
         ").await?;
